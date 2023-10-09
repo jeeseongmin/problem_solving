6b7c3d19dae3e11ce98f6758544b7aaa0576d28e
jeeseongmin <peration0422@exntu.com>
2023-10-09 23:20:27 +0900

    BOJ : 2206 (yet)

---
+++ algorithm/search/2206_ready.js
@@ -0,0 +1,15 @@
+// BOJ 2206 : 벽 부수고 이동하기
+// link : https://www.acmicpc.net/problem/2206
+const path = process.platform === "linux" ? "/dev/stdin" : "../../sample.txt";
+// 1.하나의 값을 입력받을 때
+// const input = require('fs').readFileSync(path).toString().trim();
+// const input = require('fs').readFileSync(path).toString().trim();
+// 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
+// const input = require('fs').readFileSync(path).toString().trim().split(' ');
+//[n, ...arr] = require('fs')
+//   .readFileSync(path)
+//   .toString()
+//   .trim()
+//   .split('n')
+
+console.log(input);
