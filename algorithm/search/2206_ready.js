3e96d0cf433a1ac44b56bac3fd77ae3808f451f9
jeeseongmin <peration0422@exntu.com>
2023-10-09 23:24:21 +0900

    BOJ : 2206 (yet)

--- algorithm/search/2206_ready.js
+++ algorithm/search/2206_ready.js
@@ -5,11 +5,8 @@ const path = process.platform === "linux" ? "/dev/stdin" : "../../sample.txt";
 // const input = require('fs').readFileSync(path).toString().trim();
 // const input = require('fs').readFileSync(path).toString().trim();
 // 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
-// const input = require('fs').readFileSync(path).toString().trim().split(' ');
-//[n, ...arr] = require('fs')
-//   .readFileSync(path)
-//   .toString()
-//   .trim()
-//   .split('n')
 
-console.log(input);
+[a, ...arr] = require("fs").readFileSync(path).toString().trim().split("\n");
+const [N, M] = a.split(" ").map(Number);
+arr = arr.map((item, index) => item.split("").map(Number));
+console.log(N, M, arr);
