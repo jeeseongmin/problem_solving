557d01ffb7ff4b99a2ed8efc8de9d0b4b841c1cf
jeeseongmin <peration0422@exntu.com>
2023-10-10 23:17:42 +0900

    BOJ : 2206 (yet)

--- algorithm/search/2206_ready.js
+++ algorithm/search/2206_ready.js
@@ -9,4 +9,21 @@ const path = process.platform === "linux" ? "/dev/stdin" : "../../sample.txt";
 [a, ...arr] = require("fs").readFileSync(path).toString().trim().split("\n");
 const [N, M] = a.split(" ").map(Number);
 arr = arr.map((item, index) => item.split("").map(Number));
-console.log(N, M, arr);
+
+/**
+ * 1. board 값이 0이고 visited가 0일 때 , dfs 진행
+ *    이때 visited는 1로 체크
+ * 2. 결국 x, y가 n-1, m-1에 도달할 때 결국 min과 비교한다.
+ */
+function solution(n, m, board) {
+  let visited = Array.from({ length: n }, () =>
+    Array.from({ length: m }, () => 0)
+  );
+
+  console.log(visited);
+  dfs(0, 0, board, visited, 1);
+}
+
+function dfs(x, y, _board, _visited, op) {}
+
+console.log(solution(N, M, arr));
