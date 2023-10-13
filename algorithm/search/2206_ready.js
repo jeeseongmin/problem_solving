bce853070d73b6121a780e07da395f5b87683f67
jeeseongmin <peration0422@exntu.com>
2023-10-13 11:30:21 +0900

    BOJ : 2206 (yet)

--- algorithm/search/2206_ready.js
+++ algorithm/search/2206_ready.js
@@ -1,15 +1,14 @@
 // BOJ 2206 : 벽 부수고 이동하기
 // link : https://www.acmicpc.net/problem/2206
 const path = process.platform === "linux" ? "/dev/stdin" : "../../sample.txt";
-// 1.하나의 값을 입력받을 때
-// const input = require('fs').readFileSync(path).toString().trim();
-// const input = require('fs').readFileSync(path).toString().trim();
-// 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
 
 [a, ...arr] = require("fs").readFileSync(path).toString().trim().split("\n");
 const [N, M] = a.split(" ").map(Number);
 arr = arr.map((item, index) => item.split("").map(Number));
 let Min = N * M;
+
+const dx = [0, 0, 1, -1];
+const dy = [1, -1, 0, 0];
 
 /**
  * 1. board 값이 0이고 visited가 0일 때 , dfs 진행
@@ -17,23 +16,49 @@ let Min = N * M;
  * 2. 결국 x, y가 n-1, m-1에 도달할 때 결국 min과 비교한다.
  */
 function solution(n, m, board) {
+  let queue = [];
   let visited = Array.from({ length: n }, () =>
     Array.from({ length: m }, () => 0)
   );
 
-  console.log(visited);
-  dfs(0, 0, board, visited, 1, 0);
+  queue.push([0, 0, 1, 1]);
+
+  while (queue.length > 0) {
+    const [x, y, op, len] = queue.shift();
+    console.log(x, y);
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+    }
+  }
 }
 
-function dfs(x, y, _board, _visited, op, len) {
-  _visited[x][y] = 1;
-  len++;
-  if (x === N - 1 && y === M - 1) Min = len < Min ? len : Min;
+// function dfs(x, y, _board, _visited, op, len) {
+//   console.log(x, y, op);
+//   _visited[x][y] = 1;
+//   len++;
+//   // 목적지에 도달한 경우 최소값이라면 변경
+//   if (x === N - 1 && y === M - 1) Min = len < Min ? len : Min;
 
-  for (let i = 0; i < 4; i++) {}
+//   for (let i = 0; i < 4; i++) {
+//     let nx = x + dx[i];
+//     let ny = y + dy[i];
+//     if (0 <= nx && nx < N && 0 <= ny && ny < M && _visited[nx][ny] === 0) {
+//       // 벽이 아닐 떄
+//       if (_board[nx][ny] === 0) {
+//         dfs(nx, ny, _board, _visited, op, len);
+//       }
+//       //  벽일 때
+//       else {
+//         // 벽을 뚫을 수 있는 기회가 있다면 시도
+//         if (op === 1) {
+//           dfs(nx, ny, _board, _visited, op - 1, len);
+//         }
+//       }
+//     }
+//   }
+// }
 
-  let dx = [0, 0, 1, -1];
-  let dy = [1, -1, 0, 0];
-}
+solution(N, M, arr);
 
-console.log(solution(N, M, arr));
+console.log(Min === N * M ? -1 : Min);
